b46a734721456408a1e00f3b6c9b3bcdf73ede33
Mateusz.T <mateusz.trzepacz@gmail.com>
2017-11-14 13:21:39 +0100

    initial

---
+++ src/Quick/Quick.java
@@ -0,0 +1,14 @@
+package Quick;
+
+public class Quick {
+public static void main(String[] args)
+{
+	String s = "0";
+	for (int i = 0; i < 5; i++) 
+	{
+	s = s + i;
+	} 
+	System.out.println(s);
+}
+
+}
